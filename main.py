a1c0e108c281e43e6949a4767e1aa0ad603e3da2
hectorog6612 <159784158+hectorog6612@users.noreply.github.com>
2024-03-13 16:14:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -187,7 +187,7 @@ async def account_login(bot: Client, m: Message):
                     try:
                         ka = await helper.download(url, name)
                         copy = await bot.send_document(chat_id=m.chat.id,document=ka, caption=cc1)
-                        await copy.copy(chat_id = -1001784009370)
+                        await copy.copy(chat_id = -1002036679367)
                         count+=1
                         os.remove(ka)
                         time.sleep(1)
@@ -201,7 +201,7 @@ async def account_login(bot: Client, m: Message):
                         download_cmd = f"{cmd} -R 25 --fragment-retries 25"
                         os.system(download_cmd)
                         copy = await bot.send_document(chat_id=m.chat.id,document=f'{name}.pdf', caption=cc1)
-                        await copy.copy(chat_id = --1002036679367)
+                        await copy.copy(chat_id = -1002036679367)
                         count += 1
                         os.remove(f'{name}.pdf')
                     except FloodWait as e:
